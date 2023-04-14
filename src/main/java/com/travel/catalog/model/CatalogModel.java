6c460f47037be2e02ca197723a4ca7e99c5286d1
jokopurwanto <jokopurwanto1996@yahoo.com>
2023-04-14 08:58:01 +0700

    update table dan field

--- src/main/java/com/travel/catalog/model/CatalogModel.java
+++ src/main/java/com/travel/catalog/model/CatalogModel.java
@@ -2,27 +2,37 @@ package com.travel.catalog.model;
 
 import jakarta.persistence.*;
 
+import java.sql.Timestamp;
+
 @Entity
-@Table(name = "packages")
+@Table(name = "tb_packages")
 public class CatalogModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name ="id")
     private Integer id;
 
-    @Column(name = "name")
+    @Column(name = "destination")
     private String name;
 
     @Column(name = "price")
     private String price;
 
+    @Column(name = "availability")
+    private Integer availability;
+
+    @Column(name = "date")
+    private Timestamp date;
+
     public CatalogModel() {
     }
 
-    public CatalogModel(Integer id, String name, String price) {
+    public CatalogModel(Integer id, String name, String price, Integer availability, Timestamp date) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.availability = availability;
+        this.date = date;
     }
 
     public Integer getId() {
@@ -47,6 +57,22 @@ public class CatalogModel {
 
     public void setPrice(String price) {
         this.price = price;
+    }
+
+    public Integer getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(Integer availability) {
+        this.availability = availability;
+    }
+
+    public Timestamp getDate() {
+        return date;
+    }
+
+    public void setDate(Timestamp date) {
+        this.date = date;
     }
 }
 
